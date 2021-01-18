1bf5c3f1f945f1550672464f784c32a4a0824b83
floordiv <pavloromanenko25@gmail.com>
2021-01-18 03:09:36 +0200

    core.semantic.parsers: added function, fcall, and varassign parsers

---
+++ core/semantic/parsers.py
@@ -0,0 +1,57 @@
+from core.objects.tools import split_tokens
+from core.objects.types import COMMA, VARASSIGN, EXPR
+from core.objects.classes import (Function, FunctionCall, VarAssign)
+
+
+def function(parser, tokens):
+    _, name, raw_args, raw_body = tokens
+    body = parser(raw_body.value)
+    args, kwargs = _parse_args(parser, raw_args.value)
+
+    return Function(name.value, args, kwargs, body)
+
+
+def function_call(parser, tokens):
+    name, raw_args = tokens
+    args, kwargs = _parse_args(parser, raw_args.value)
+
+    return FunctionCall(name.value, args, kwargs)
+
+
+def var_assign(parser, tokens):
+    var, _, *raw_val = tokens
+    val = parser(raw_val)[0]
+
+    if val.type == EXPR:
+        val = val.value[0]
+
+    return VarAssign(var, val)
+
+
+def _parse_args(parser, args, leave_tokens=True):
+    if not args:
+        return [], {}
+
+    elements = split_tokens(args, COMMA)
+    args = []
+    kwargs = {}
+
+    for element in elements:
+        parsed_element = parser(element)[0]
+
+        if parsed_element.type != VARASSIGN:    # to accept all the structures inside
+            arg = parsed_element.value[0]
+
+            if not leave_tokens:
+                arg = arg.value
+
+            args.append(arg)
+        else:   # this is kwarg
+            val = element.val
+
+            if not leave_tokens:
+                val = val.value
+
+            kwargs[element.var.value] = val
+
+    return args, kwargs
