48ef9189cbf4b880e82bdf9fc258124574f7a555
floordiv <pavloromanenko25@gmail.com>
2021-01-23 01:03:00 +0200

    core.transpiler.python.forms: added form for return statement and condition branches; edited function and function-call forms

--- core/transpiler/python/forms.py
+++ core/transpiler/python/forms.py
@@ -1,14 +1,19 @@
-from core.objects.types import FUNCTION, FUNCTION_CALL, VARASSIGN
+from core.objects.types import (FUNCTION, FUNCTION_CALL, VARASSIGN,
+                                RETURN_STATEMENT)
 
 
-function_form = """def {name}({args}):
-    {code}"""
-function_call_form = "{name}({args})"
+function_form = """def {name}({args}):\n{code}"""
+function_call_form = "{name}({args})\n"
 varassign_form = "{var} = {val}"
+if_branch_form = "if {expr}:\n{code}"
+elif_branch_form = "elif {expr}:\n{code}"
+else_branch_form = "else:\n{code}"
+return_form = "return {val}\n"
 
 
 forms = {
     FUNCTION: function_form,
     FUNCTION_CALL: function_call_form,
     VARASSIGN: varassign_form,
+    RETURN_STATEMENT: return_form,
 }
