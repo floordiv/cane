b62c92f390dba14c9778d334259cd3b3321023f5
floordiv <pavloromanenko25@gmail.com>
2021-02-04 18:11:34 +0200

    test: added for-loop, while-loop tests (not tests, just to check whether no errors occur)

--- test.py
+++ test.py
@@ -9,6 +9,16 @@ func my_func() {
 
 response = my_func()
 print(response)
+
+for (i=0; i<5; i+=1) {
+    print('hello,', i)
+}
+
+i = 0
+
+while (i < 5) {
+    print('i is', i)
+}
 """
 
 lexer = Lexer(code)
