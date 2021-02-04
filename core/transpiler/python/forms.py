d309c0e87af5ced647e8063457526a0485dd927d
floordiv <pavloromanenko25@gmail.com>
2021-02-04 18:10:37 +0200

    core.transpiler.python.forms: again using newlines in the end of the form (to control indents before and after more correctly)

--- core/transpiler/python/forms.py
+++ core/transpiler/python/forms.py
@@ -1,19 +1,20 @@
-from core.objects.types import (FUNCTION, FUNCTION_CALL, VARASSIGN,
-                                RETURN_STATEMENT)
+import core.objects.types as types
 
-
-function_form = """def {name}({args}):\n{code}"""
+function_form = "def {name}({args}):\n{code}\n"
 function_call_form = "{name}({args})\n"
-varassign_form = "{var} = {val}"
-if_branch_form = "if {expr}:\n{code}"
-elif_branch_form = "elif {expr}:\n{code}"
-else_branch_form = "else:\n{code}"
+varassign_form = "{var} = {val}\n"
+if_branch_form = "if {expr}:\n{code}\n"
+elif_branch_form = "elif {expr}:\n{code}\n"
+else_branch_form = "else:\n{code}\n"
 return_form = "return {val}\n"
-
+break_form = "break"
+continue_form = "continue"
 
 forms = {
-    FUNCTION: function_form,
-    FUNCTION_CALL: function_call_form,
-    VARASSIGN: varassign_form,
-    RETURN_STATEMENT: return_form,
+    types.FUNCTION: function_form,
+    types.FUNCTION_CALL: function_call_form,
+    types.VARASSIGN: varassign_form,
+    types.RETURN_STATEMENT: return_form,
+    types.BREAK_STATEMENT: break_form,
+    types.CONTINUE_STATEMENT: continue_form
 }
