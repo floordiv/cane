ee76b3d9c58fe9b4c42c885f6e8e9d8658db7bff
floordiv <pavloromanenko25@gmail.com>
2021-02-15 17:44:46 +0200

    core.interpreter.scopes: added skeleton for future scopes

--- core/interpreter/scopes.py
+++ core/interpreter/scopes.py
@@ -1,6 +1,22 @@
-
+from core.objects.classes import Variable
+from core.interpreter.eval import evaluate
 
 
 class Scope:
     def __init__(self, init_vars=None):
-        ...
+        if init_vars is None:
+            init_vars = {}
+
+        self.variables = init_vars
+
+    def get(self, var):
+        if isinstance(var, Variable):
+            return get_var(var)
+        elif not isinstance(var, str):
+            return evaluate(var)
+
+        return self.variables[var]
+
+
+def get_var(var):
+    ...
